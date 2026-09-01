f556365df1bf74ac7d37c64ce73cb69870973f91
VerangaOseah <oseahhaeso@gmail.com>
2026-09-01 10:19:56 +0800

    Add files via upload

--- code_challenge2.py
+++ code_challenge2.py
@@ -13,11 +13,21 @@ h = 5
 i = 1
 
 print("The amount of",a,"in",money,"is",money // a)
+money = money % a
 print("The amount of",b,"in",money,"is",money // b)
+money = money % b
 print("The amount of",c,"in",money,"is",money // c)
+money = money % c
 print("The amount of",d,"in",money,"is",money // d)
+money = money % d
 print("The amount of",e,"in",money,"is",money // e)
+money = money % e
 print("The amount of",f,"in",money,"is",money // f)
+money = money % f
 print("The amount of",g,"in",money,"is",money // g)
+money = money % g
 print("The amount of",h,"in",money,"is",money // h)
+money = money % h
 print("The amount of",i,"in",money,"is",money // i)
+money = money % i
+
