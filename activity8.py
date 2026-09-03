77b5f8e1b7a37f993f97cedbde50c9d6f19be201
VerangaOseah <oseahhaeso@gmail.com>
2026-09-03 11:27:41 +0800

    Add files via upload

--- activity8.py
+++ activity8.py
@@ -1,7 +1,11 @@
 #hobbies
 hobbies = ""
-print("My hobbies are ", hb)
+hb = input("What are your hobbies? ")
+print("My hobbies are ", hb,)
 hobbies += hb
-hb input("what else?")
+hb = input("what else?")
+hobbies += hb + ","
+print("My hobbies are ", hobbies)
+hb = input("what else?")
 hobbies += hb + ","
 print("My hobbies are ", hobbies)
