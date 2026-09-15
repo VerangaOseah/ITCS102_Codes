96d669922dc5ab8feb2d07a339f4938760125bac
VerangaOseah <oseahhaeso@gmail.com>
2026-09-15 11:21:14 +0800

    Add files via upload

---
+++ activity14.py
@@ -0,0 +1,32 @@
+age = int(input("Enter your age: "))
+is_employed = bool(input("Are you currently employed? "))
+credit_score = float(input("Credit Score: "))
+annual_income = float(input("What is your annual Salary? "))
+has_collateral = bool(input("Do you have any collateral? (True/False) "))
+base_rate = 0.0
+
+if age >= 21 and is_employed == True:
+        print("Approved")
+        if credit_score >= 750: #Tier1
+         print("You have a high credit score")
+         if annual_income >= 100000:
+                base_rate = 4.5
+                print("Your base rate is ", base_rate)
+        elif credit_score >= 600 and credit_score < 750:
+              print("Your credit score is less than 750")
+              if has_collateral == True:
+                    print("You have collateral")
+                    base_rate = 7.0
+                    print("Your base rate is ", base_rate)
+                elif annual_income <= 40000:
+                        print("Low annual income")
+                        base_rate = 9.5
+                        print("Your base rate is ", base_rate)
+                else:
+                     base_rate = 8.0
+                     print("Your base rate is ", base_rate)
+        elif credit_score < 600:
+                print("Rejected: Credit score is too low")
+
+else:
+                print("Denied")
